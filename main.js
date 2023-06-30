89f5a9aa5f9d926a33114244ba978c35ef2eafdb
kirill <kirll.boy@yandex.ru>
2023-06-30 21:41:10 +0300

    first commit

--- main.js
+++
@@ -1,9 +0,0 @@
-const element = document.querySelector('.registration');
-
-element.addEventListener('mouseover', function() {
-    element.classList.add('hover');
-});
-
-element.addEventListener('mouseout', function() {
-    element.classList.remove('hover');
-});
