00e2a4c8da263993a28747b51a4174725f27f009
boychenko <boychenko@tutu.tech>
2023-07-02 11:24:56 +0300

    fix modal

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 const modalButton = document.querySelector('.footer__text');
 const modal = document.querySelector('.modal');
-const modal__content = document.querySelector('.modal__content');
+const modal__inner = document.querySelector('.modal__inner');
 const body = document.querySelector('body');
 const closeButton = document.querySelector('.closeButton');
 
 const closeModal = () => {
-    modal__content.classList.remove('modal__content--open');
+    modal__inner.classList.remove('modal__content--open');
     modal.classList.remove('modal--open');
     body.style.overflow = 'unset';
 }
 
 modalButton.onclick = () => {
-    modal__content.classList.add('modal__content--open');
+    modal__inner.classList.add('modal__content--open');
     modal.classList.add('modal--open');
     body.style.overflow = 'hidden';
 }
 
 modal.onclick = (e) => {
     if (e.target !== modal) return;
+    closeModal();
+}
+
+modal__inner.onclick = (e) => {
+    if (e.target !== modal__inner) return;
     closeModal();
 }
 
